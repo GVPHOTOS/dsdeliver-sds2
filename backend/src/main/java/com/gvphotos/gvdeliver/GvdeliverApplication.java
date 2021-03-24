9af32ccd7f920d27370f79531e7f4281119ffd0d
unknown <gvphotosedcoes@gmail.com>
2021-03-23 23:33:40 -0300

    Projetct created

---
+++ backend/src/main/java/com/gvphotos/gvdeliver/GvdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.gvphotos.gvdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gvphotos/gvdeliver/GvdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvphotos.gvdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
