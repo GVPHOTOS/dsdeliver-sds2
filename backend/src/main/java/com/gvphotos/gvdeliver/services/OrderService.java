f66fda64f2fa7e84844e6fd08d8b6f7112cc87c0
unknown <gvphotosedcoes@gmail.com>
2021-03-27 00:04:15 -0300

    GET /Orders

---
+++ backend/src/main/java/com/gvphotos/gvdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.gvphotos.gvdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gvphotos.gvdeliver.dto.OrderDTO;
+import com.gvphotos.gvdeliver.entities.Order;
+import com.gvphotos.gvdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}	
+}
